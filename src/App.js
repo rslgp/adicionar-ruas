186ce55eff9ae40a2eb44f0e97d2b98bfd8e8503
rslgp <rslgp@cin.ufpe.br>
2023-09-16 09:09:03 -0300

    change from typescript to js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StreetAutocomplete from './StreetAutocomplete'
+
+function App() {
+  return (
+    <div className="App">
+      <StreetAutocomplete/>
+    </div>
+  );
+}
+
+export default App;
